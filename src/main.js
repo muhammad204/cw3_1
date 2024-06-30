4eac08f4e6f444fe72ba870f0e6142db4db258f1
MI7 <muhammadidris204@gmail.com>
2024-06-30 09:06:15 +0400

    deployment

--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import '../public/css/bootstrap.css'
-import '../public/css/bootstrap.min.css'
-import '../public/css/style.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import '../public/css/bootstrap.css';
+import '../public/css/bootstrap.min.css';
+import '../public/css/style.css';
 
-createApp(App).mount('#app')
+// WebSocket client code
+const socket = new WebSocket('ws://10.33.25.17:8080/ws');
+
+socket.onopen = () => {
+    console.log('WebSocket is open now.');
+};
+
+socket.onmessage = (event) => {
+    console.log('WebSocket message received:', event.data);
+};
+
+socket.onerror = (error) => {
+    console.error('WebSocket error observed:', error);
+};
+
+socket.onclose = () => {
+    console.log('WebSocket is closed now.');
+};
+
+createApp(App).mount('#app');
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "cw3",
+  publicPath: "cw3_1",
 })
